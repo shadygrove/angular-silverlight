272962d546234406eb165bcaf1fb66a0b5c6ac81
Martin Bour <martin.bour@philips.com>
2015-04-21 11:10:55 -0400

    initial commit

---
+++ SilverlightWithAngular/Globals.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightWithAngular
+{
+    public class Globals
+    {
+        private static IDictionary<string, string> _initParams;
+
+        public static void SetInitParams(IDictionary<string, string> initParams)
+        {
+            _initParams = initParams;
+        }
+
+        public static string ScriptObject
+        {
+            get { return _initParams["scriptObjectType"]; }
+        }
+
+        public static string ScriptObject_Reload
+        {
+            get { return _initParams["scriptObjectReload"]; }
+        }
+    }
+}
---
+++ SilverlightWithAngular/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Browser;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightWithAngular
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string scriptObjName = Globals.ScriptObject;
+            string scriptObjReload = Globals.ScriptObject_Reload;
+
+            // Get instance of the script object
+            ScriptObject so = (ScriptObject)HtmlPage.Window.Invoke(scriptObjName);
+            so.Invoke(scriptObjReload);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            throw new Exception("You make me puke!");
+        }
+    }
+}
